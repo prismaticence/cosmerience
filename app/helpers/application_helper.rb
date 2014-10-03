df7637485f136c801e70e779d5654b0586f2707c
prismaticence <cuddlelooker@gmail.com>
2014-10-03 09:37:21 +0800

    3 oct

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,11 +14,13 @@ module ApplicationHelper
 	end
 
    def nested_comments(comments)
-
      comments.map do |comment, sub_comments|
-   
+ 	
        render(comment) + content_tag(:div, nested_comments(sub_comments), :class => "nested_comments")
-     end.join.html_safe
+     
+       end.join.html_safe
+  
+    
    end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
 	has_many :reviews
 	has_many :comments
 	has_secure_password
-  validates_confirmation_of :password
+    validates_confirmation_of :password
 
 	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "40x40>" }, :default_url => "/images/:style/missing.png"
   	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
